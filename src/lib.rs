1c9b1bf63f333c1538184af54cb2b4924b56bdf6
clarkenciel <clarkenciel@gmail.com>
2018-11-11 20:20:11 -0800

    🚧✨ connection logic figured out

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap,HashSet};
+use std::collections::{HashMap};
 
 struct Performer {
     id: PerformerId,
@@ -7,32 +7,146 @@ struct Performer {
 }
 
 impl Performer {
-    fn speaking_area(&self) -> Sector {
-    }
+    fn speaking_areas(&self) -> (Sector, Sector) {}
 
-    fn hearing_areas(&self) -> (Sector, Sector) {
-    }
+    fn hearing_areas(&self) -> (Sector, Sector) {}
 }
 
 type PerformerId = u64;
 
-
-type Performers = HashSet<Performer>;
+type Performers = HashMap<PerformerId, Performer>;
 
 struct PerformerGraph {
     edges: HashMap<PerformerId, Edges>,
-    performers: HashMap<PerformerId, Performer>, // feels like a big redundancy
+    performers: Performers, // feels like a big redundancy
+}
+
+struct Slots {
+    right: Option<PerformerId>,
+    left: Option<PerformerId>,
+}
+
+impl Slots {
+    fn available_on(&self, s: &Side) -> bool {
+        match s {
+            Side::Left => self.left.is_none(),
+            Side::Right => self.right.is_none(),
+        }
+    }
 }
 
 struct Edges {
-    ears: (Option<PerformerId>, Option<PerformerId>),
-    mouth: (Option<PerformerId>, Option<PerformerId>),
+    ears: Ears,
+    mouth: Mouth,
 }
 
-fn connectable(p1: &Performer,  p2: &Performer) -> bool {
-    let audience = p1.speaking_area();
+struct Mouth(Slots);
+
+struct Ears(Slots);
+
+enum Side {
+    Left,
+    Right,
+}
+
+struct Connection {
+    source: PerformerId,
+    source_side: Side,
+    sink: PerformerId,
+    sink_side: Side,
+}
+
+trait Source {
+    fn output_free(&self, &Side) -> bool;
+}
+
+impl Source for Mouth {
+    fn output_free(&self, s: &Side) -> bool {
+        match self {
+            Mouth(slots) => slots.available_on(s),
+        }
+    }
+}
+
+trait Sink {
+    fn input_free(&self, &Side) -> bool;
+}
+
+impl Sink for Ears {
+    fn input_free(&self, s: &Side) -> bool {
+        match self {
+            Ears(slots) => slots.available_on(s),
+        }
+    }
+}
+
+impl Connection {
+    fn can_connect(&self, m: &Source, e: &Sink) -> bool {
+        m.output_free(&self.source_side) && e.input_free(&self.sink_side)
+    }
+}
+
+// return connections that can be made given a set of edges
+fn possible_connections(cs: Vec<Connection>, edges: &HashMap<PerformerId, Edges>) -> Vec<Connection> {
+    cs.iter().filter(|&c| connection_is_possible(c, &edges)).map(|&c| c).collect()
+}
+
+// a connection can be made in an edge set under the following conditions:
+//   1. neither source nor sink exist in the edge set yet
+//   2. source exists, has an available mouth slot and sink does not exist
+//   3. sink exists, has an available ear slot and source does not exist
+//   4. source and sink exist, connection can connect them
+fn connection_is_possible(c: &Connection, edges: &HashMap<PerformerId, Edges>) -> bool {
+    match (edges.get(&c.source), edges.get(&c.sink)) {
+        (None, None) => true,
+        (Some(source), None) => source.mouth.output_free(&c.source_side),
+        (None, Some(sink)) => sink.ears.input_free(&c.sink_side),
+        (Some(source), Some(sink)) => c.can_connect(&source.mouth, &sink.ears),
+    }
+}
+
+fn connections(p1: &Performer, p2: &Performer) -> Vec<Connection> {
+    let (left_audience, right_audience) = p1.speaking_areas();
     let (audible_l, audible_r) = p2.hearing_areas();
-    audience.contains(&p2.position) && (audience.intersects(&audible_l) || audience.intersects(&audible_r))
+    let mut connections = vec![];
+
+    if left_audience.contains(&p2.position) && audible_l.contains(&p1.position) {
+        connections.push(Connection {
+            source: p1.id,
+            source_side: Side::Left,
+            sink: p2.id,
+            sink_side: Side::Left,
+        })
+    }
+
+    if left_audience.contains(&p2.position) && audible_r.contains(&p1.position) {
+        connections.push(Connection {
+            source: p1.id,
+            source_side: Side::Left,
+            sink: p2.id,
+            sink_side: Side::Right,
+        })
+    }
+
+    if right_audience.contains(&p2.position) && audible_l.contains(&p1.position) {
+        connections.push(Connection {
+            source: p1.id,
+            source_side: Side::Right,
+            sink: p2.id,
+            sink_side: Side::Left,
+        })
+    }
+
+    if right_audience.contains(&p2.position) && audible_r.contains(&p1.position) {
+        connections.push(Connection {
+            source: p1.id,
+            source_side: Side::Right,
+            sink: p2.id,
+            sink_side: Side::Right,
+        })
+    }
+
+    connections
 }
 
 struct Sector {
@@ -45,10 +159,8 @@ struct Sector {
 impl Sector {
     fn contains(&self, p: &Position) -> bool {
         let vec = p.displacement(&self.center);
-        self.end.clockwise_of(&vec) && vec.clockwise_of(&self.start) && vec.magnitude() < self.radius
-    }
-
-    fn intersects(&self, other: &Self) -> bool {
+        self.end.clockwise_of(&vec) && vec.clockwise_of(&self.start) &&
+            vec.magnitude() < self.radius
     }
 }
 
@@ -56,17 +168,15 @@ struct Position(f64, f64);
 
 impl Position {
     fn displacement(&self, other: &Position) -> Vector {
-        Vector(
-            other.0 - self.0,
-            other.1 - self.1,
-        )
+        Vector(other.0 - self.0, other.1 - self.1)
     }
 }
 
 struct Vector(f64, f64);
 
 impl Vector {
-    // taken from: https://stackoverflow.com/questions/13652518/efficiently-find-points-inside-a-circle-sector#13675772
+    // taken from:
+    // https://stackoverflow.com/questions/13652518/efficiently-find-points-inside-a-circle-sector#13675772
     fn clockwise_of(&self, other: &Vector) -> bool {
         self.inverse_normal().dot(other) <= 0f64
     }
